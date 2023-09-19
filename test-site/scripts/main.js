48d150903d14bcbad67d93cf55ca4cf0ee0b3f05
Ecksluss <145249002+Ecksluss@users.noreply.github.com>
2023-09-19 10:29:37 -0500

    Update main.js

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -27,7 +27,7 @@ if (!localStorage.getItem("name")) {
   setUserName();
 } else {
   const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  myHeading.textContent = `Please, follow me on Patreon, ${storedName}`;
 }
 
 myButton.onclick = () => {
